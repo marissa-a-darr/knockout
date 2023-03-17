b2646c265ea247cdcb2cbf8f676cfb404e237c2a
lizramos611 <liz.ramos@aim.com>
2023-03-16 20:34:59 -0400

    deleted extra folders

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import User from './User';
 
 const Profile = () => {
   return (
@@ -7,6 +8,18 @@ const Profile = () => {
         This is the Profile Page!<br /><br />
 
         <Link to="/edit_profile">Edit Profile</Link><br /><br />
+
+        <div className="welcomeback">Welcome Back, {User}!</div>
+
+
+        <div className="profilepic">profile picture goes here</div>
+
+        <div className="myteams">List of teams goes here:
+       <li>
+        <ul className="teaminfo">Team Name, your role, location</ul>
+
+        </li> 
+        </div>
     </div>
   );
 };
