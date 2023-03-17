9aaf132e1fd2d9a6e56266134b982237e20d24d4
lizramos611 <liz.ramos@aim.com>
2023-03-16 20:21:14 -0400

    adding in layout for profile

---
+++ client copy/src/pages/AddTeam.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddTeam = () => {
+  return (
+    <div className="container">
+        This is the Add Team Page!
+    </div>
+  );
+};
+
+export default AddTeam;
---
+++ client copy/src/pages/CreateAccount.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateAccount = () => {
+  return (
+    <div className="container">
+        This is the Create Account Page!
+    </div>
+  );
+};
+
+export default CreateAccount;
