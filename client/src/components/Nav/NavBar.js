428ba6e7a7d4152857178f9d5ecf45fd7f2377db
Marissa Darr <mrsaandee@gmail.com>
2023-03-23 19:58:04 -0400

    "wrote auth0, and header bar with working links"

---
+++ client/src/components/Nav/NavBar.js
@@ -0,0 +1,28 @@
+import { Stack } from "@chakra-ui/react";
+import React from "react";
+import MenuLinks from "./MenuLinks";
+import Logo from "./Logo";
+import NavBarContainer from "./NavBarContainer";
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    
+    <NavBarContainer {...props}>
+      <Logo w="10px" color={["white", "white", "primary.500", "primary.500"]} />
+     
+      <Stack
+        spacing={8}
+        align="center"
+        justify={["center", "space-between", "flex-end", "flex-end"]}
+        direction={["column", "row", "row", "row"]}
+        pt={[4, 4, 0, 0]}
+      >
+       <MenuLinks />
+      </Stack>
+    </NavBarContainer>
+  );
+};
+export default NavBar;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
 import User from './User';
 
 const Profile = () => {
+  const {user, isAuthenticated} = useAuth0();
   return (
+    isAuthenticated && (
     <div className="container">
         This is the Profile Page!<br /><br />
 
         <Link to="/edit_profile">Edit Profile</Link><br /><br />
 
-        <div className="welcomeback">Welcome Back, {User}!</div>
+        <div className="welcomeback">Welcome Back, {user.name}!</div>
 
 
-        <div className="profilepic">profile picture goes here</div>
+        <div className="profilepic"> <img src={user.picture} alt={user.name} /></div>
+        
+        <div className= "useremail">{user.email}</div>
 
         <div className="myteams">List of teams goes here:
        <li>
@@ -21,6 +26,7 @@ const Profile = () => {
         </li> 
         </div>
     </div>
+  )
   );
 };
 
