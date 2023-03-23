0308d3dade51c93889a3c5de4e95d392e1972499
lizramos611 <liz.ramos@aim.com>
2023-03-23 18:42:39 -0400

    finishing uploading images button

--- client/src/pages/EditProfile.js
+++ client/src/pages/EditProfile.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {  Link } from "react-router-dom";
-import { Button, Flex, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { Button,  FormControl,  Input, Text } from "@chakra-ui/react";
 const myStyle = {
-
+color: "white", 
   backgroundColor: "#000000",
   padding: "20px",
-  fontSize: "50px",
+
   fontFamily: "Lucida Console, Monaco, monospace",
 height: "100%",
 
@@ -14,16 +14,23 @@ height: "100%",
 const EditProfile = () => {
   return (
     <div className="container" style={myStyle}>
-         <Flex gap={"20px"}>
+         
           <FormControl>
          
-      
-            <Input type="text" maxlength="5" placeholder='Enter Your Zipcode' height={"70px"} width={"250px"} color="white"/>
-            <Input type="text" placeholder="Enter something about yourself!" height={"250px"} color="white" />
+         <Input type={"text"}  placeholder="Edit Your Name" height={"80px"} width={"250px"}/>
+         <br />
+            <Input type="text" maxlength="5" placeholder='Enter Your Zipcode' height={"80px"} width={"250px"} color="white"/>
+            <br />
+          
+            <Input type="text" placeholder="Enter something about yourself for your bio!" height={"250px"} color="white" />
+            <br />
+ 
+            <Text fontSize={"30px"}>Upload a Photo for your Profile!</Text> <input type="file" accept="image/*"  fontSize="25px" />
+            <br />
           </FormControl>
-         </Flex>
+       <br />
 
-        <Link to="/profile"><Button color={"gold"}>Save Profile</Button></Link><br /><br />
+        <Link to="/profile"><Button color={"black"}>Save Profile</Button></Link><br /><br />
     </div>
   );
 };
