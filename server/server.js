66d0611d5bc984c96f67e9692582edf09b49097f
Marissa Darr <mrsaandee@gmail.com>
2023-03-27 18:17:06 -0400

    changed localhost to heroku

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const http = require('http').Server(app);
 const cors = require('cors');
 const socketIO = require('socket.io')(http, {
   cors: {
-      origin: "http://localhost:3000"
+      origin: "https://knockout-app.herokuapp.com/"
   }
 });
 
