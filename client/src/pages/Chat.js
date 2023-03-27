5830f433cbed4bb63d53f9a0da33320bd5ae336d
Antonio De Filippis <113878094+Quaz711@users.noreply.github.com>
2023-03-27 01:28:29 -0400

    chat features integrated

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,19 +1,32 @@
-import React from "react";
-const myStyle = {
-  color: "#ffffff",
-  backgroundColor: "#000000",
-  padding: "20px",
-  fontSize: "20px",
-  fontFamily: "Lucida Console, Monaco, monospace",
-  minHeight: "100vh"
+import React, { useState, useEffect } from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import ChatPage from '../components/ChatContatiner/ChatPage';
+import socketIO from 'socket.io-client';
+// import NewWindow from 'react-new-window';
 
-}
+const socket = socketIO.connect('http://localhost:3001');
+// window.addEventListener("beforeunload", (ev) => 
+// {  
+//     console.log("window is being close");
+//   ev.preventDefault();
+//     return ev.returnValue = 'Are you sure you want to close?';
+// });
+
 const Chat = () => {
-  console.log("this is the chat")
+  const {user} = useAuth0();
+  const [userName, setUserName] = useState('');
+  console.log(user);
+  localStorage.setItem('userName', user.nickname);
+  useEffect(()=>{
+		setUserName(user.nickname);
+	}, [user.nickname]);
+
+  socket.emit('newUser', { userName, socketID: socket.id });
+  
   return (
-    <div className="container" style={myStyle}>
-        This is the Chat Page!
-    </div>
+    // <NewWindow>
+        <ChatPage socket={socket} />
+    // </NewWindow>
   );
 };
 
